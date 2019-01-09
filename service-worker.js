04313a5949f77b8ca3064f46ce227a61871d6644
Edgar Mueller <emueller@eclipsesource.com>
2019-01-09 17:21:32 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.836a8f0a1f4b1269753488f39ec5b6b6.js"
+  "/precache-manifest.3eb0ac95e276604bfe07659c68add733.js"
 );
 
 workbox.clientsClaim();
